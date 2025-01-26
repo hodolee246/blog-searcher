b7a969ba5dcbe20bbb3034224a332ca2284c7ec5
hodolee246 <hodolee246@naver.com>
2025-01-26 19:28:04 +0900

    feat : 블로그 검색 캐시추가

---
+++ infrastructure/src/main/java/com/hodolee/example/config/CacheConfig.java
@@ -0,0 +1,47 @@
+package com.hodolee.example.config;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.cache.support.CompositeCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+    @Bean
+    public CacheManager compositeCacheManager(CacheManager caffeineCacheManager, CacheManager redisCacheManager) {
+        return new CompositeCacheManager(caffeineCacheManager, redisCacheManager);
+    }
+
+    @Bean
+    public CacheManager caffeineCacheManager() {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+        cacheManager.setCaffeine(Caffeine.newBuilder()
+                .expireAfterWrite(5, TimeUnit.MINUTES)
+                .maximumSize(1000));
+        return cacheManager;
+    }
+
+    @Bean
+    public CacheManager redisCacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration cacheConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(10))
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()));
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(cacheConfig)
+                .build();
+    }
+
+}
--- infrastructure/src/main/java/com/hodolee/example/service/BlogSearcherService.java
+++ infrastructure/src/main/java/com/hodolee/example/service/BlogSearcherService.java
@@ -1,17 +1,15 @@
 package com.hodolee.example.service;
 
 import com.hodolee.example.searcher.BlogSearcher;
-import com.hodolee.example.searcher.dto.ExternalApiResponse;
-import com.hodolee.example.searcher.dto.kakao.KakaoApiResponseDto;
 import com.hodolee.example.searcher.dto.BlogSearchDto;
-import com.hodolee.example.searcher.impl.KakaoSearcher;
-import com.hodolee.example.searcher.dto.naver.NaverBlogResponseDto;
-import com.hodolee.example.searcher.impl.NaverSearcher;
+import com.hodolee.example.searcher.dto.ExternalApiResponse;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cache.CacheManager;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
 
 @Slf4j
 @Service
@@ -20,27 +18,60 @@ public class BlogSearcherService {
     private final BlogSearcher kakaoSearcher;
     private final BlogSearcher naverSearcher;
     private final SearchHistoryService searchHistoryService;
+    private final CacheManager cacheManager;
 
     public BlogSearcherService(@Qualifier("kakaoSearcher") BlogSearcher kakaoSearcher,
                                @Qualifier("naverSearcher") BlogSearcher naverSeacher,
-                               SearchHistoryService searchHistoryService) {
+                               SearchHistoryService searchHistoryService,
+                               CacheManager cacheManager) {
         this.kakaoSearcher = kakaoSearcher;
         this.naverSearcher = naverSeacher;
         this.searchHistoryService = searchHistoryService;
+        this.cacheManager = cacheManager;
     }
 
     @CircuitBreaker(name = "caller", fallbackMethod = "getNaverBlog")
     public ExternalApiResponse getKakaoBlog(String query, String sort, Integer page) {
+        ExternalApiResponse cachedResponse = getCachedResponse(query);
+        if (cachedResponse != null) {
+            log.info("getKakaoBlog() / query: {}", query);
+            return cachedResponse;
+        }
+
         searchHistoryService.saveSearchHistory(query);
-        return kakaoSearcher.searchBlog(new BlogSearchDto(query, sort, page));
+        ExternalApiResponse response = kakaoSearcher.searchBlog(new BlogSearchDto(query, sort, page));
+        saveToCache("blogSearchCache:kakao", query, response);
+        return response;
     }
 
     private ExternalApiResponse getNaverBlog(String query, String sort, Integer page, Throwable t) {
-        log.info("Fallback : {}", t.getMessage());
+        ExternalApiResponse cachedResponse = getCachedResponse(query);
+        if (cachedResponse != null) {
+            log.info("getNaverBlog() / query: {}", query);
+            return cachedResponse;
+        }
+
         searchHistoryService.saveSearchHistory(query);
         BlogSearchDto blogSearchDto = new BlogSearchDto(query, sort, page);
         blogSearchDto.convertNaverApi();
-        return naverSearcher.searchBlog(blogSearchDto);
+        ExternalApiResponse response = naverSearcher.searchBlog(blogSearchDto);
+        saveToCache("blogSearchCache:naver", query, response);
+        return response;
+    }
+
+    private ExternalApiResponse getCachedResponse(String query) {
+        ExternalApiResponse kakaoCache = Objects.requireNonNull(cacheManager.getCache("blogSearchCache:kakao"))
+                .get(query, ExternalApiResponse.class);
+        if (kakaoCache != null) {
+            return kakaoCache;
+        }
+
+        return Objects.requireNonNull(cacheManager.getCache("blogSearchCache:kakao"))
+                .get(query, ExternalApiResponse.class);
+    }
+
+    private void saveToCache(String cacheName, String query, ExternalApiResponse response) {
+        Objects.requireNonNull(cacheManager.getCache(cacheName)).put(query, response);
     }
 
 }
