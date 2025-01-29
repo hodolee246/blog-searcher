bbd1ad5d9d5ce5947e701f35f5bd41428cb85ea5
hodolee246 <hodolee246@naver.com>
2025-01-29 23:23:31 +0900

    modify : 캐시 가져오는 부분 카카오, 네이버 구분추가

--- infrastructure/src/main/java/com/hodolee/example/service/BlogSearcherService.java
+++ infrastructure/src/main/java/com/hodolee/example/service/BlogSearcherService.java
@@ -10,6 +10,7 @@ import org.springframework.cache.CacheManager;
 import org.springframework.stereotype.Service;
 
 import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
 
 @Slf4j
 @Service
@@ -32,7 +33,7 @@ public class BlogSearcherService {
 
     @CircuitBreaker(name = "caller", fallbackMethod = "getNaverBlog")
     public ExternalApiResponse getKakaoBlog(String query, String sort, Integer page) {
-        ExternalApiResponse cachedResponse = getCachedResponse(query);
+        ExternalApiResponse cachedResponse = getCachedResponse("kakao", query, sort, page);
         if (cachedResponse != null) {
             log.info("getKakaoBlog() / query: {}", query);
             return cachedResponse;
@@ -47,7 +48,7 @@ public class BlogSearcherService {
 
     private ExternalApiResponse getNaverBlog(String query, String sort, Integer page, Throwable t) {
         log.info("circuit break / message : {}", t.getMessage());
-        ExternalApiResponse cachedResponse = getCachedResponse(query);
+        ExternalApiResponse cachedResponse = getCachedResponse("naver", query, sort, page);
         if (cachedResponse != null) {
             log.info("getNaverBlog() / query: {}", query);
             return cachedResponse;
@@ -62,15 +63,29 @@ public class BlogSearcherService {
         return response;
     }
 
-    private ExternalApiResponse getCachedResponse(String query) {
-        ExternalApiResponse kakaoCache = Objects.requireNonNull(cacheManager.getCache("blogSearchCache:kakao"))
-                .get(query, ExternalApiResponse.class);
-        if (kakaoCache != null) {
-            return kakaoCache;
+    private ExternalApiResponse getCachedResponse(String blog, String query, String sort, Integer page) {
+        if ("kakao".equals(blog)) {
+            ExternalApiResponse kakaoCache = cacheManager.getCache("blogSearchCache:kakao")
+                    .get(query, ExternalApiResponse.class);
+            if (kakaoCache != null) {
+                CompletableFuture.runAsync(() -> {
+                    ExternalApiResponse updatedResponse = kakaoSearcher.searchBlog(new BlogSearchDto(query, sort, page));
+                    saveToCache("blogSearchCache:kakao", query, updatedResponse);
+                });
+                return kakaoCache;
+            }
+        } else {
+            ExternalApiResponse naverCache = cacheManager.getCache("blogSearchCache:naver")
+                    .get(query, ExternalApiResponse.class);
+            if (naverCache != null) {
+                CompletableFuture.runAsync(() -> {
+                    ExternalApiResponse updatedResponse = kakaoSearcher.searchBlog(new BlogSearchDto(query, sort, page));
+                    saveToCache("blogSearchCache:naver", query, updatedResponse);
+                });
+                return naverCache;
+            }
         }
-
-        return Objects.requireNonNull(cacheManager.getCache("blogSearchCache:naver"))
-                .get(query, ExternalApiResponse.class);
+        return null;
     }
 
     private void saveToCache(String cacheName, String query, ExternalApiResponse response) {
